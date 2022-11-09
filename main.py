59a87b397a46fa57e6faaeb43b117dae91dd3301
Bareaa <guilhermebarea0@gmail.com>
2022-11-09 11:24:13 -0300

    mobilidade e tamanhos corrigidos, música adicionada, som de morte e comando de fim de jogo e drop de bigorna no random

--- main.py
+++ main.py
@@ -1,93 +1,118 @@
-import os
 import pygame
 import random
+import time
 pygame.init()
-os.system("cls")
-print("Inicio do jogo!")
-largura = 470
+pygameDisplay = pygame.display
+pygameDisplay.set_caption("teste de jogo")
 altura = 612
-tamanho = (largura,altura) #tupla - imutável -> não pode alterar os valores
-display = pygame.display.set_mode( tamanho )
-fps = pygame.time.Clock()
-pygame.display.set_caption("gamePygame")
-tamanhoXjogador = 50
-tamanhoYjogador = 50
-posicaojogadorX = 202
-posicaojogadorY = 455
-movimentojogadorX = 0
-movimentojogadorY = 0
-velocidade = 3.5
-posicaoX=433 #X vai crescendo da esquerda para direita 0 --> 470
-posicaoY=00 #Y vai crescendo de baixo para cima 0 --> 612
-velocidadequeda = 4.56
+largura = 453
+tamanho = (largura, altura)
+gameDisplay = pygame.display.set_mode(tamanho)
+clock = pygame.time.Clock()
+gameEvents = pygame.event
 branco = (255,255,255)
-
-rodando = True  # looping para ficar rodando o jogo
-
-sopa = pygame.image.load("assets/macarrao.png")
-pirulito = pygame.image.load("assets/pirulito.png")
-toddynho = pygame.image.load("assets/toddynho.png")
+vermelho = (255,0,0)
 
 fundo = pygame.image.load("assets/fundo.jpg")
 jogador = pygame.image.load("assets/clarencio.png")
-jogador = pygame.transform.scale(jogador, (155, 155)) #redimensionar uma imagem
-sopa = pygame.transform.scale(sopa, (40, 60))
-pirulito = pygame.transform.scale(pirulito, (40, 60))
-toddynho = pygame.transform.scale(toddynho, (40, 50))
+bigorna = pygame.image.load("assets/bigorna.png")
 
-# pontuacao = falta fazer o esquema de pontuação
-pontuacao = 0
+bigorna = pygame.transform.scale(bigorna, (50, 60))
+jogador = pygame.transform.scale(jogador, (100, 130))
 
-
-def escreverTexto (texto):
-    fonte  = pygame.font.Font("freesansbold.ttf",15)
-    textoDisplay = fonte.render(texto,True,branco)
-    display.blit(textoDisplay, (5,5))
-
-def alimentos (posicaoalimentoX, posicaoalimentoY):
-    display.blit(sopa, (random.randint(posicaoalimentoX, posicaoalimentoY)))
-    
-
-pygame.mixer.music.load("assets/lo-fi.mp3")
+pygame.mixer.music.load("assets/lo-fi.wav")
 pygame.mixer.music.play(-1) #looping infinito (-1)
 pygame.mixer.music.set_volume(0.35)
 
 
 
-while rodando:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            rodando = False
-        elif evento.type == pygame.KEYDOWN:
-            if evento.key == pygame.K_a:
-                movimentojogadorX = velocidade * -1
-            elif evento.key == pygame.K_d:
-                movimentojogadorX = velocidade
-        elif evento.type == pygame.KEYUP:
-            movimentojogadorX = 0
+
+def escreverTexto (texto):
+    fonte  = pygame.font.Font("freesansbold.ttf",15)
+    textoDisplay = fonte.render(texto,True,branco)
+    gameDisplay.blit(textoDisplay, (00,00))
+    pygameDisplay.update()
+
+def morreu():
+    fonte  = pygame.font.Font("freesansbold.ttf",45)
+    fonte2  = pygame.font.Font("freesansbold.ttf",30)
+    textoDisplay = fonte.render("Fim de jogo",True,vermelho)
+    textoDisplay2 = fonte2.render("press enter to continue !!!!",True,branco)
+    gameDisplay.blit(textoDisplay, (100,150))
+    gameDisplay.blit(textoDisplay2, (30,200))
+    pygameDisplay.update()
+
+def jogar():
+    jogando = True
+    ironX = 50
+    ironY = 480
+    movimentoIronX = 0
+    larguraIron = 77
+    alturaIron = 110
+    alturabigorna = 70
+    largurabigorna = 50
+    posicaobigornaX = 400
+    posicaobigornaY = -240
+    velocidadebigorna = 10
+    pontos = 0
     
-    #FAZER UM BLOCO COM COLISÃO DO LADO DIREITO DA TELA PARA O CLARENCIO NÃO SE ESCONDER NELA
-    #pra não sair da tela (antigo - limite de bordas)
-    if posicaojogadorX + movimentojogadorX < largura and posicaojogadorX + movimentojogadorX > 0:
-        posicaojogadorX = posicaojogadorX + movimentojogadorX
+    morte = pygame.mixer.Sound("assets/morte.mp3")
+    morte.set_volume(1)
     
-    posicaoY += velocidadequeda
-    if (posicaoY >= -200):
-            posicaoY = 433 
-    display.blit(fundo , (0,0) )
-    posicao = (posicaojogadorX,posicaojogadorY) 
-    display.blit(jogador , (posicaojogadorX,posicaojogadorY) )
-    display.blit(sopa, (posicaoX,posicaoY))
-    display.blit(pirulito, (300,posicaoY))
-    display.blit(toddynho, (120,posicaoY))
-    parede = pygame.draw.rect(display, (0,0,0), (468,00,10,800))#parede, precisamos por comando de colisão!
+    while True:
+        for event in gameEvents.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    movimentoIronX = -15
+                elif event.key == pygame.K_d:
+                    movimentoIronX = 15
+                elif event.key == pygame.K_RETURN:
+                    jogar()
+            elif event.type == pygame.KEYUP:
+                movimentoIronX = 0
+            
+        if jogando:
+            if posicaobigornaY > altura:
+                posicaobigornaY = -240
+                posicaobigornaX = random.randint(0,largura)
+                velocidadebigorna = velocidadebigorna + 1
+                pontos = pontos + 1
+                
+                
+            else:
+                posicaobigornaY =posicaobigornaY + velocidadebigorna
 
-    # if parede.colliderect(jogador): 
-    #     velocidade=0
+            if ironX + movimentoIronX >0 and ironX + movimentoIronX< largura-larguraIron:
+                ironX = ironX + movimentoIronX
+            gameDisplay.fill(branco)
+            gameDisplay.blit(fundo,(0,0))
+            gameDisplay.blit(jogador, (ironX,ironY))
+            
+            gameDisplay.blit(bigorna, (posicaobigornaX,posicaobigornaY))
+            # escreverTexto("Pontos: "+str(pontos))
+
+            pixelsXIron = list(range(ironX, ironX+larguraIron))
+            pixelsYIron = list(range(ironY, ironY+alturaIron))
+
+            pixelXbigorna = list(range(posicaobigornaX, posicaobigornaX+largurabigorna))
+            pixelYbigorna = list(range(posicaobigornaY, posicaobigornaY+alturabigorna))
+
+            colisaoY = len(list(set(pixelYbigorna) & set(pixelsYIron) ))
+            if colisaoY > 0:
+                colisaoX = len(list(set(pixelXbigorna) & set(pixelsXIron) ))
+                print(colisaoX)
+                if colisaoX > 39:
+                    morreu()
+                    jogando=False
+                    pygame.mixer.music.stop()
+                    pygame.mixer.Sound.play(morte)
 
 
-    pygame.display.update()
-    fps.tick(60)
+        pygameDisplay.update()
+        clock.tick(60)
 
+jogar()
 
-print("Volte Sempre!")
